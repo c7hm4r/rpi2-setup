f01b057ac9e1078fcd379b8452b79bf7fae37d4b
Christoph Müller <iblzm@hotmail.de>
2017-08-25 23:32:56 +0200

    wrap

--- setup.sh
+++ setup.sh
@@ -1,21 +1,43 @@
 #!/bin/bash
 
-set -e
 set -x
 
-cd $HOME
+export repo_url=https://github.com/c7hm4r/rpi2-setup.git
+export dest_dir=rpi2-setup
+
+bash <<'EOF'
+set -e
+set -x
 
 sudo apt-get update
 sudo apt-get install --yes ansible
 
-if [ ! -d rpi2-setup ]; then
-    git clone https://github.com/c7hm4r/rpi2-setup.git rpi2-setup
-    cd rpi2-setup
+if [ ! -d rpi2-setup ]
+then
+    git clone "$repo_url" "$repo_name"
+    cd "$repo_name"
 else
-    cd rpi2-setup
-    git stash --include-untracked --keep-index
-    git pull
-    git stash pop --index
-fi
+    cd "$dest_dir"
 
-bash
+    repo_modified=0
+    if ! git ls-files --other --directory --exclude-standard \
+            --no-empty-directory | sed q1 || \
+        ! git diff-files --quiet --ignore-submodules -- || \
+        ! git diff-index --cached --quiet HEAD --ignore-submodules --
+    then
+        repo_modified=1
+    fi
+
+    if [ "$repo_modified" -eq 1 ]
+    then
+        git stash --include-untracked --keep-index
+    fi
+    git pull
+    if [ "$repo_modified" -eq 1 ]
+    then
+        git stash pop --index
+    fi
+fi
+EOF && "Result: Configuration successful" || echo "Result: An error occured"
+
+cd "$dest_dir"
